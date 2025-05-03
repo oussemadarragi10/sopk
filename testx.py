0298b23b2e2b4321a5aba65d1c413da92a0578ae
oussemadarragi10 <143362220+oussemadarragi10@users.noreply.github.com>
2025-05-03 15:02:18 +0200

    Update testx.py

--- testx.py
+++ testx.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "your-secret-key-here"  # Change this to a strong secret key
 
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)  # Token expiration time
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = "mongodb://mongo:LCrIUkdHYJGHOaWeVakXmXzeuTLysgKa@nozomi.proxy.rlwy.net:24828"
 mongo = PyMongo(app)
 
 jwt = JWTManager(app)
